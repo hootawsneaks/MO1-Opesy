794e7766204865b8a56aea2a83fd31ae63014695
hootawsneaks <162249452+hootawsneaks@users.noreply.github.com>
2026-06-08 00:19:16 +0800

    Directory Modifications

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+﻿#include "menu.h"
+
+int main()
+{
+	std::cout << logo << std::endl;
+	std::cout << "Developers: Kean Carvin, Gideon Chua, Daniel Pua, Bea Uy";
+	return 0;
+}
---
+++ src/menu.h
@@ -0,0 +1,23 @@
+﻿// MO1 Opesy.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <string>
+
+const std::string logo = R"(
+ _______  _______ _________   _______  _______  _______ _________ _____  
+(  ____ \(  ___  )\__   __/  (  ____ )(  ___  )(  ___  )\__   __// ___ \ 
+| (    \/| (   ) |   ) (     | (    )|| (   ) || (   ) |   ) (  ( (   ) )
+| |      | |   | |   | |     | (____)|| |   | || |   | |   | |   \/  / / 
+| | ____ | |   | |   | |     |     __)| |   | || |   | |   | |      ( (  
+| | \_  )| |   | |   | |     | (\ (   | |   | || |   | |   | |      | |  
+| (___) || (___) |   | |     | ) \ \__| (___) || (___) |   | |      (_)  
+(_______)(_______)   )_(     |/   \__/(_______)(_______)   )_(       _   
+                                                                    (_) 
+)";
+
+const std::string motd = "0.0.0";
+
+// TODO: Reference additional headers your program requires here.
