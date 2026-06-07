9d44206b48da98620eb512aeacf354bbf13ce556
hootawsneaks <162249452+hootawsneaks@users.noreply.github.com>
2026-06-07 19:53:52 +0800

    Add project files.

---
+++ MO1 Opesy/MO1 Opesy.cpp
@@ -0,0 +1,12 @@
+﻿// MO1 Opesy.cpp : Defines the entry point for the application.
+//
+
+#include "MO1 Opesy.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ MO1 Opesy/MO1 Opesy.h
@@ -0,0 +1,8 @@
+﻿// MO1 Opesy.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
