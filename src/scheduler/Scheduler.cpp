3393cb91b2671833b3f3344bab5aeb58a71f470a
bea-nsprout <beaantoinette26@gmail.com>
2026-06-26 02:51:42 +0800

    RR: added scheduler config variables

--- src/scheduler/Scheduler.cpp
+++ src/scheduler/Scheduler.cpp
@@ -4,3 +4,17 @@ void enqueue(Scheduler& sched, Process& p) {
 	std::unique_lock<std::mutex> lock(sched.readyMutex);
 	sched.readyQueue.push(&p);
 }
+
+/**
+* returns -1 if it wasn't successful, but returns 1 if was successfully set
+*/
+int setSchedulerConfig(Scheduler& sched, Config& config) {
+	if (config.scheduler == "\"fcfs\"") sched.schedulerType = FCFS;
+	else if (config.scheduler == "\"rr\"") sched.schedulerType = RR;
+	else return -1;
+
+	sched.quantumCycles = config.quantumCycles;
+	sched.delaysPerExec = config.delaysPerExec;
+
+	return 1;
+}
--- src/scheduler/Scheduler.h
+++ src/scheduler/Scheduler.h
@@ -4,6 +4,9 @@
 #include <vector>
 #include <condition_variable>
 #include "process/Process.h"
+#include "core/config.h"
+
+enum SchedulerType { FCFS, RR };
 
 struct Scheduler {
 	std::queue<Process*> readyQueue;
@@ -14,6 +17,11 @@ struct Scheduler {
 	std::mutex coreProcessesMutex;
 	std::mutex finishedMutex;
 	std::condition_variable cv;
+	SchedulerType schedulerType;
+	int quantumCycles;
+	int delaysPerExec;
 };
 
 void enqueue(Scheduler& sched, Process& p);
+
+int setSchedulerConfig(Scheduler& sched, Config& config);
